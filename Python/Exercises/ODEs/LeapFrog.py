b442c0fe69f4034dd76820a2943004f2e75518df
Nennni <francesco.a.iraci@gmail.com>
2020-11-14 17:52:25 +0100

    Leap Frog completed

--- Python/Exercises/ODEs/LeapFrog.py
+++ Python/Exercises/ODEs/LeapFrog.py
@@ -2,11 +2,9 @@ import numpy as np
 from numpy import linalg
 import matplotlib.pyplot as plt
 
-def a(x1,x2,m):
-	f = -G*m*(x1-x2)/((abs(linalg.norm(x1-x2)))**3)
+def a(x,y):
+	f = -(x-y)/(np.linalg.norm(x-y))**3
 	return f
-
-
 
 G = 1. #Gravity constant
 t_0 = 0.0
@@ -24,16 +22,19 @@ X[1,0,0], X[1,1,0] = -1.,-1.
 V[0,0,0],V[0,1,0] = -0.5,0.
 V[1,0,0],V[1,1,0] = 0.5,0.
 
+b = a(X[0,:,0],X[1,:,0])
 
-
-for k in range(int(N)):
+print(b)
+for k in range(1,int(N)):
 	for i in range(2):
 		for j in range(2):
 			if (j!=i):
-				X[i,:,k] = X[i,:,k-1]+h*V[i,:,k-1] +0.5*h*h*(a(X[i,:,k-1],X[j,:,k-1],m[j]))
-				V[i,:,k] = V[i,:,k-1]+0.5*h*(a(X[i,:,k-1],X[j,:,k-1],m[j]) + a(X[i,:,k],X[j,:,k],m[j]))
+				X[i,:,k] = X[i,:,k-1]+h*V[i,:,k-1] +0.5*h*h*(a(X[i,:,k-1],X[j,:,k-1]))
+				X[j,:,k] = X[j,:,k-1]+h*V[j,:,k-1] +0.5*h*h*(a(X[j,:,k-1],X[i,:,k-1]))
+				V[i,:,k] = V[i,:,k-1]+0.5*h*(a(X[i,:,k-1],X[j,:,k-1]) + a(X[i,:,k],X[j,:,k]))
 
-grafic = plt.plot(X[0,0,:],X[0,1,:],"r-",label='Leap Frog')
+grafic = plt.plot(X[0,0,:],X[0,1,:],"r-",label='m1, Leap Frog')
+grafic1 = plt.plot(X[1,0,:],X[1,1,:],"r-",label='m2, Leap Frog')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.legend()
