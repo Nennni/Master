3cd83575aa0b7ed3c39551de25af803cf7d1c995
Nennni <francesco.a.iraci@gmail.com>
2020-11-23 16:23:07 +0100

    Completed

--- Mathematical and Numerical Methods/Python/Exercises/ODEs/ModifiedMidPoint.py
+++ Mathematical and Numerical Methods/Python/Exercises/ODEs/ModifiedMidPoint.py
@@ -12,19 +12,19 @@ h = 0.01
 N = int((t_fin - t_0)/h)
 H = int(t_fin-t_0)
 t = np.arange(t_0,t_fin,h)
+
 x = np.zeros(N,float) #integer multiple of h
 x[0] = x_t0
 y = np.zeros(N,float) #fractionary multiple of h
-y[0] = x_t0
+y[1] = x[0] + 0.5*h*f(x[0],t_0)
+x[1] = x[0] +h*f(y[1],0.5*h)
 
-for i in range(1,N): #index of x,y
-	y[i] = y[i-1] + 0.5*h*f(x[i-1],(i-1)*h)	
-	x[i] = x[i-1] + h*f(y[i],(i-1+0.5)*h)
-	X.append(y[i])
-	X.append(x[i])
+for i in range(1,N-1): #index of x,y
+	y[i+1] = y[i] + h*f(x[i],i*h)	
+	x[i+1] = x[i] + h*f(y[i+1],(i+0.5)*h)
 x[N-1] = 0.5*(x[N-1]+y[N-1]+0.5*h*f(x[N-1],t_fin))
 
-graph = plt.plot(t,X,"b-")
+graph = plt.plot(t,x,"b-")
 plt.xlabel('t')
 plt.ylabel('x(t)')
 plt.tight_layout()
